3dd09ce4bc0f4d61de5443b5cc4df2814ddf8e8e
Andrew Silva <andrew.agoravai@gmail.com>
2024-08-22 17:15:02 -0300

    feat: create and add Students to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
-function App() {}
+import Student from "./Student";
+
+function App() {
+  return (
+    <>
+      <Student name="Andrew" age={20} isStudent={true} />
+      <Student name="Patrick" age={32} isStudent={false} />
+      <Student name="Bob" age={25} isStudent={true} />
+    </>
+  );
+}
 
 export default App;
