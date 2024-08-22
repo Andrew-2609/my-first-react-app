d01fc1c631f348be7c0bde77f622713f110f22cd
Andrew Silva <andrew.agoravai@gmail.com>
2024-08-21 22:21:34 -0300

    style: stylize card image

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,11 @@ import profilePicture from "./assets/mec-man.png";
 function Card() {
   return (
     <div className="card">
-      <img alt="My avatar profile picture" src={profilePicture} />
+      <img
+        className="card-image"
+        alt="My avatar profile picture"
+        src={profilePicture}
+      />
       <h2>Ndrewcoding</h2>
       <p>Software Engineer and History Nerd</p>
     </div>
