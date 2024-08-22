5df4ec08b3da024fc1a021357daad939dc4df5b2
Andrew Silva <andrew.agoravai@gmail.com>
2024-08-22 17:25:59 -0300

    feat: add default props to Student component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <Student name="Andrew" age={20} isStudent={true} />
       <Student name="Patrick" age={32} isStudent={false} />
       <Student name="Bob" age={25} isStudent={true} />
+      <Student />
     </>
   );
 }
