d2d336c072c6a94b8482c33883dc347968bf9135
Andrew Silva <andrew.agoravai@gmail.com>
2024-08-24 11:32:04 -0300

    feat: use useEffect to add and remove event listeners

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,32 +1,32 @@
 import { useEffect, useState } from "react";
 
 function MyComponent() {
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState("green");
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
 
   useEffect(() => {
-    document.title = `Count: ${count} ${color}`;
-  }, [count, color]);
+    window.addEventListener("resize", handleResize);
+    console.log("Event added");
 
-  const addCount = () => {
-    setCount((c) => c + 1);
-  };
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-  const subtractCount = () => {
-    setCount((c) => c - 1);
-  };
+  useEffect(() => {
+    document.title = `${width}x${height}`;
+  }, [width, height]);
 
-  const changeColor = () => {
-    setColor((c) => (c === "green" ? "red" : "green"));
-  };
+  function handleResize() {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
 
   return (
     <>
-      <p style={{ color: color }}>Count: {count}</p>
-      <button onClick={addCount}>Add</button>
-      <button onClick={subtractCount}>Subtract</button>
-      <br />
-      <button onClick={changeColor}>Change Color</button>
+      <p>
+        Window Resolution: {width}x{height}
+      </p>
     </>
   );
 }
