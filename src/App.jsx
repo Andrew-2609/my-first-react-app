58032c93e6c39e4ce8d96b5a1c1e22d17a97da72
Andrew Silva <andrew.agoravai@gmail.com>
2024-08-24 12:33:04 -0300

    feat: implement digital clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
-function App() {}
+import DigitalClock from "./DigitalClock";
+
+function App() {
+  return <DigitalClock />;
+}
 
 export default App;
---
+++ src/DigitalClock.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+function DigitalClock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const formatTime = () => {
+    const padZero = (number) => {
+      return number < 10 ? "0" + number : number;
+    };
+
+    let hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+
+    hours = hours % 12 || 12;
+
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(
+      seconds
+    )} ${meridiem}`;
+  };
+
+  return (
+    <div className="clock">
+      <span>{formatTime()}</span>
+    </div>
+  );
+}
+
+export default DigitalClock;
